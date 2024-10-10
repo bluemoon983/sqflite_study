e58f50cfd791e5f9538c6d792f582ff6232f9a61
bluemoon983 <msdrobot@naver.com>
2024-10-10 17:48:23 +0900

    detail_view

--- lib/src/detail_view.dart
+++ lib/src/detail_view.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
 
+class DetailView extends StatefulWidget {
+  final int sample;
+  const DetailView({super.key, required this.sample});
+
+  @override
+  State<DetailView> createState() => _DetailViewState();
+}
+
+class _DetailViewState extends State<DetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.sample.toString())),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "name: 대상혁 ${widget.sample}",
+              style: const TextStyle(fontSize: 15),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Y/N : GOAT',
+              style: TextStyle(fontSize: 15),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'value : 4',
+              style: TextStyle(fontSize: 15),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "CreatedAt: ${DateTime.now()}",
+              style: const TextStyle(fontSize: 15),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+              onPressed: () {},
+              child: const Text(
+                'Update',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () {},
+              child: const Text(
+                'Delete',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/main_screens.dart
+++ lib/src/main_screens.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/src/detail_view.dart';
 
-class MainScreens extends StatelessWidget {
+class MainScreens extends StatefulWidget {
   const MainScreens({super.key});
 
+  @override
+  State<MainScreens> createState() => _MainScreensState();
+}
+
+class _MainScreensState extends State<MainScreens> {
   Widget _sampleOne(int index) {
-    return Container(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 10,
-                height: 10,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.green),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => DetailView(sample: index)));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(22),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(
+                  width: 7,
+                ),
+                Text('제길 또 대상혁야 나는 숭배해야만 해 #$index'),
+              ],
+            ),
+            const SizedBox(height: 7),
+            Text(
+              DateTime.now().toIso8601String(),
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
               ),
-              Text('제길 또 대상혁야 나는 숭배해야만 해 #$index'),
-            ],
-          ),
-          const SizedBox(height: 7),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -30,8 +56,9 @@ class MainScreens extends StatelessWidget {
       appBar: AppBar(
         title: const Text("SQFlite Sample"),
       ),
-      body:
-          ListView(children: List.generate(100, (index) => _sampleOne(index))),
+      body: ListView(
+        children: List.generate(100, (index) => _sampleOne(index)),
+      ),
     );
   }
 }
