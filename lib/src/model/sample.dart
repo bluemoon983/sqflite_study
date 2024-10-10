74a5efa0ba580415b84ad0291241bc7522b3a499
bluemoon983 <msdrobot@naver.com>
2024-10-10 18:00:46 +0900

    Sample

--- lib/src/model/sample.dart
+++ lib/src/model/sample.dart
@@ -1,1 +1,15 @@
+class Sample {
+  final int? id;
+  final String name;
+  final bool yn;
+  final double value;
+  final DateTime createdAt;
 
+  const Sample({
+    required this.id,
+    required this.name,
+    required this.yn,
+    required this.value,
+    required this.createdAt,
+  });
+}
